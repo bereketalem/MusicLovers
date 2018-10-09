74a177365bb9ae7aa0aa55887b3f4cdd96bfed1b
bereketbruk <bereketbruk@outlook.com>
2018-10-09 20:50:18 +0200

    create add a Gig form

---
+++ GitHub/Controllers/GigsController.cs
@@ -0,0 +1,26 @@
+﻿using GitHub.Models;
+using GitHub.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GitHub.Controllers
+{
+    public class GigsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Create()
+        {
+            var obj = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+            return View(obj);
+        }
+    }
+}
